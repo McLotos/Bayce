0ff9b6c7614045832e702b5c18e885a23ceaa96e
McLotos <mclotos@gmail.com>
2015-07-09 11:28:23 +0600

    Create example.php

---
+++ example.php
@@ -0,0 +1,30 @@
+require 'ratings.class.php';
+ 
+ $db = new dBase();
+ 
+ $sql = 'SELECT * FROM elems';
+ $bankList = $db->select($sql, array());
+ 
+ $sql = 'SELECT `id`,`elem_id`, `vote`, `vote_date` FROM ratings WHERE `acepted`={?}';
+ $allRatings = $db->select($sql, array('1'));
+ 
+ $table=array(
+                 'elem'=>'elem_id',
+                 'rating'=>'vote',
+                 'date'=>'vote_date',
+                 'eId'=>'id',
+                 'eName'=>'name'
+              );
+                       
+ $rating = new Rating($table, $bankList, $allRatings, 90, 10);
+
+ $test['count']['zeroStars'] = $rating->getCount('2','0');
+ $test['count']['oneStars'] = $rating->getCount('2','1');
+ $test['count']['twoStars'] = $rating->getCount('2','2');
+ $test['count']['threeStars'] = $rating->getCount('2','3');
+ $test['count']['fourStars'] = $rating->getCount('2','4');
+ $test['count']['fiveStars'] = $rating->getCount('2','5');
+ $test['count']['all'] = $rating->getCount('2','all');
+ $test['count']['accepted'] = $rating->getCount('2','accepted');
+ $test['oldRating'] = $rating->getCount('2','oldRating');
+ $test['newRating'] = $rating->getCount('2','newRating');
